c938ac39509d8db3688fd475e37c597a5d7c1a21
lou-PL-dev <louiseplessis@gmail.com>
2026-09-02 07:58:06 +0200

    Track anonymous app opens for tester journeys

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import FriendFlow from './FriendFlow.jsx';
 import AdminTestSessions from './AdminTestSessions.jsx';
+import { getOrCreateUserId, trackEvent } from './api';
 import './index.css';
 
 const friendToken = new URLSearchParams(window.location.search).get('f');
 const isAdminSessions = window.location.pathname.replace(/\/$/, '') === '/admin/test-sessions';
+
+if (!friendToken && !isAdminSessions) {
+  const uid = getOrCreateUserId();
+  trackEvent(uid, 'app_opened');
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
